f1db7726f4846da3a49a464be616aa3c1a991dfe
Chacron1 <git@chacron.one>
2026-08-10 23:35:05 +0200

    Views: Apple Credentials: Validate credentials

--- client/models/apple_api_key.gd
+++ client/models/apple_api_key.gd
@@ -4,10 +4,29 @@ class AppleApiKey:
 	var issuer_id: String
 	var p8: String                  ## PEM contents of the .p8 file
 
-	func _init(key_id_: String = "", issuer_id_: String = "", p8_: String = "") -> void:
-		key_id = key_id_
-		issuer_id = issuer_id_
-		p8 = p8_
+	# Key ID: 10 uppercase alphanumeric chars, e.g. "2X9R4HXF34"
+	var _key_id_regex := RegEx.create_from_string(r"^[A-Z0-9]{10}$")
+	# Issuer ID: lowercase UUID, e.g. "57246542-96fe-1a63-e053-0824d011072a"
+	var _issuer_id_regex := RegEx.create_from_string(r"^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$")
+	# .p8 contents: PKCS#8 PEM block. Group 1 = base64 body (may span lines).
+	var _p8_regex := RegEx.create_from_string(r"^\s*-----BEGIN PRIVATE KEY-----\s*([A-Za-z0-9+/=\s]+?)\s*-----END PRIVATE KEY-----\s*$")
+
+	func _init(key_id: String = "", issuer_id: String = "", p8: String = "") -> void:
+		key_id = key_id
+		issuer_id = issuer_id
+		p8 = p8
 
 	func to_dict() -> Dictionary:
 		return {"key_id": key_id, "issuer_id": issuer_id, "p8": p8}
+
+
+	func validate_key_id() -> bool:
+		return _key_id_regex.search(key_id) != null
+
+
+	func validate_issuer_id() -> bool:
+		return _issuer_id_regex.search(issuer_id) != null
+
+
+	func validate_p8() -> bool:
+		return _p8_regex.search(p8) != null
--- views/apple_credentials/apple_credentials.gd
+++ views/apple_credentials/apple_credentials.gd
@@ -6,13 +6,22 @@ const ActivateResponse = preload("res://addons/shipgodot_ios/client/models/activ
 const SGModalData = preload("res://addons/shipgodot_ios/components/modal/sg_modal_data.gd")
 
 
-var bundle_id_scene = preload("res://addons/shipgodot_ios/views/project_setup/project_setup.tscn")
+var project_setup_scene = preload("res://addons/shipgodot_ios/views/project_setup/project_setup.tscn")
 
 var apple_team_id : String = ""
 var apple_key : AppleApiKey
 var dialog : FileDialog
 
 @export var security_info : SGModalData
+
+@export_multiline var malformed_team_id_message : String = ""
+@export_multiline var malformed_key_id_message : String = ""
+@export_multiline var malformed_issuer_id_message : String = ""
+@export_multiline var malformed_p8_key_message : String = ""
+
+
+var _team_id_regex := RegEx.create_from_string("^[A-Z0-9]{10}$")
+
 
 func _ready() -> void:
 	apple_key = AppleApiKey.new()
@@ -23,6 +32,7 @@ func _ready() -> void:
 	%P8APIKeyButton.pressed.connect(_on_load_key_button_pressed)
 	%ContinueButton.pressed.connect(_on_continue)
 	request_modal.emit(security_info)
+
 
 func _on_load_key_button_pressed() -> void:
 	dialog = FileDialog.new()
@@ -38,24 +48,61 @@ func _on_load_key_button_pressed() -> void:
 	add_child(dialog)
 	dialog.popup_centered(Vector2i(800, 600))
 
+
 func _on_key_file_selected(path: String) -> void:
 	var file := FileAccess.open(path, FileAccess.READ)
 	if file == null:
-		push_error("Could not open %s: %s" % [path, error_string(FileAccess.get_open_error())])
+		var error_msg := "Could not open %s: %s" % [path, error_string(FileAccess.get_open_error())]
 		dialog.queue_free()
+		var error_modal = SGModalData.error_modal(error_msg)
+		request_modal.emit(error_modal)
 		return
 	apple_key.p8 = file.get_as_text()
 	dialog.queue_free()
 
+
 func _on_continue() -> void:
-	apple_team_id = %TeamID.get_text()
-	apple_key.key_id = %KeyID.get_text()
-	apple_key.issuer_id = %IssuerID.get_text()
+	apple_team_id = %TeamID.get_text().strip_edges()
+	apple_key.key_id = %KeyID.get_text().strip_edges()
+	apple_key.issuer_id = %IssuerID.get_text().strip_edges()
+
+	if not _check_credentials():
+		return
+
 	processing.emit(true)
 	var act := await api.activate(api.license_key, store.get_device_id(), store.get_device_name(), apple_team_id, apple_key)
-	# TODO: check if activation is successful
 	processing.emit(false)
-	if act:
-		api.seat_token = act.seat_token
-		store.set_seat_token(act.seat_token)
-	change_view.emit(bundle_id_scene)
+
+	if not act:
+		var error_modal = SGModalData.error_modal(api.get_last_error_formatted())
+		request_modal.emit(error_modal)
+		return
+
+	api.seat_token = act.seat_token
+	store.set_seat_token(act.seat_token)
+	change_view.emit(project_setup_scene)
+
+
+# TODO: add subtler indicators that there's an error
+func _check_credentials() -> bool:
+	if _team_id_regex.search(apple_team_id) == null:
+		var error_modal = SGModalData.error_modal(malformed_team_id_message)
+		request_modal.emit(error_modal)
+		return false
+
+	if not apple_key.validate_key_id():
+		var error_modal = SGModalData.error_modal(malformed_key_id_message)
+		request_modal.emit(error_modal)
+		return false
+
+	if not apple_key.validate_issuer_id():
+		var error_modal = SGModalData.error_modal(malformed_issuer_id_message)
+		request_modal.emit(error_modal)
+		return false
+
+	if not apple_key.validate_p8():
+		var error_modal = SGModalData.error_modal(malformed_p8_key_message)
+		request_modal.emit(error_modal)
+		return false
+
+	return true
