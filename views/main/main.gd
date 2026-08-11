4d5e9b8add16d23aad1aba206c0f0e6763d159f9
Chacron1 <git@chacron.one>
2026-08-11 20:38:51 +0200

    Views: Main: Cancel builds on upload failure; Display session data

--- views/main/main.gd
+++ views/main/main.gd
@@ -2,42 +2,62 @@
 extends ShipGodotView
 
 const BuildSlot = preload("res://addons/shipgodot_ios/client/models/build_slot.gd").BuildSlot
+const BuildInfoContainer = preload("res://addons/shipgodot_ios/views/main/components/build_info_container/build_info_container.gd")
 
 const ZIP_PATH := "user://project_upload.zip"
 
 @export var build_component : PackedScene
 
 var _default_excludes := [".godot", ".git", ".import"]
+var _current_build_container : BuildInfoContainer
 
 
 func _ready() -> void:
+	update_session()
 	%RunButton.pressed.connect(_create_new_build)
 
 
-func _get_engine_version() -> String:
-	var info := Engine.get_version_info()
-	var major: int = info["major"]
-	var minor: int = info["minor"]
-	var patch: int = info["patch"]
-	var status: String = info["status"]
+func update_session() -> void:
+	var session := await api.get_session()
+	if not session:
+		_display_last_api_error()
+		return
+	%LicenseStatus.text = session.license_status
+	%BuildMinutes.text = str(session.minutes_available)
 
-	if patch > 0:
-		#return "%d.%d.%d-%s" % [major, minor, patch, status]
-		return "%d.%d.%d" % [major, minor, patch]
-	
-	#return "%d.%d-%s" % [major, minor, status]
-	return "%d.%d" % [major, minor]
 
 func _create_new_build() -> void:
+	processing.emit(true)
+
 	var slot : BuildSlot = await api.request_build_slot()
-	var build = build_component.instantiate()
-	build.api = api
-	build.build_id = slot.build_id
-	if %EmptyText:
-		%EmptyText.queue_free()
-	%BuildsContainer.add_child(build)
-	await zip_and_upload_project(slot.upload_url)
-	api.dispatch_build(slot.build_id, get_bundle_id(), _get_engine_version())
+	if not slot:
+		processing.emit(false)
+		_display_last_api_error()
+		return
+
+	%EmptyText.visible = false
+
+	if _current_build_container:
+		_current_build_container.finished.disconnect(update_session)
+
+	_current_build_container = build_component.instantiate()
+	_current_build_container.api = api
+	_current_build_container.build_id = slot.build_id
+	_current_build_container.version = str(ProjectSettings.get_setting("application/config/version", ""))
+	_current_build_container.finished.connect(update_session)
+	%BuildsContainer.add_child(_current_build_container)
+	%BuildsContainer.move_child(_current_build_container, 0)
+
+	if not await zip_and_upload_project(slot.upload_url):
+		await _current_build_container.cancel_build()
+		processing.emit(false)
+		return
+
+	if not await api.dispatch_build(slot.build_id, get_bundle_id(), _get_engine_version()):
+		_display_last_api_error()
+		await _current_build_container.cancel_build()
+	processing.emit(false)
+
 
 
 # TODO: Upload in parts
@@ -58,14 +78,18 @@ func zip_and_upload_project(presigned_url: String, excludes: Array = []) -> bool
 	if open_err != OK:
 		push_error("Could not create zip: %s" % error_string(open_err))
 		return false
+
 	for f in files:
 		packer.start_file(f.substr("res://".length()))   # relative path inside archive
 		packer.write_file(FileAccess.get_file_as_bytes(f))
 		packer.close_file()
 	packer.close()
 
-	# Upload the zip file
-	return await api.upload_project_zip(presigned_url, ZIP_PATH)
+	if not await api.upload_project_zip(presigned_url, ZIP_PATH):
+		_display_last_api_error()
+		return false
+
+	return true
 
 
 func _collect_files(dir_path: String, out: Array, excludes: Array) -> void:
--- views/shipgodot_view.gd
+++ views/shipgodot_view.gd
@@ -12,11 +12,30 @@ var api : ShipGodotClient
 var store : ShipGodotStore
 
 
+func _display_last_api_error() -> void:
+	var error_modal = SGModalData.error_modal(api.get_last_error_formatted())
+	request_modal.emit(error_modal)
+
+
 func _paste_from_clipboard(where: LineEdit) -> void:
 	var clipboard = DisplayServer.clipboard_get()
 	if clipboard.length() > 0:
 		where.text = clipboard
 
+
+static func _get_engine_version() -> String:
+	var info := Engine.get_version_info()
+	var major: int = info["major"]
+	var minor: int = info["minor"]
+	var patch: int = info["patch"]
+	var status: String = info["status"]
+
+	if patch > 0:
+		#return "%d.%d.%d-%s" % [major, minor, patch, status]
+		return "%d.%d.%d" % [major, minor, patch]
+
+	#return "%d.%d-%s" % [major, minor, status]
+	return "%d.%d" % [major, minor]
 
 static func get_bundle_id() -> String:
 	return ProjectSettings.get_setting(ShipGodotPlugin.BUNDLE_ID_SETTING, "")
